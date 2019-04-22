4cd8fa9e0f6b70d7a9e710f6e35de9ab232f53f0
yonatanB <yonatanberruecos.yb@gmail.com>
2019-04-21 19:29:54 -0500

    fix htps

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ const apikey=config.apiKey
 
 export default function getartist(country){
    
-   return fetch('http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country='+country+'&api_key='+apikey+'&format=json')
+   return fetch('https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country='+country+'&api_key='+apikey+'&format=json')
    .then(res=>res.json())
    .then(yeison=>yeison.topartists.artist)
 } 
